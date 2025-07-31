d6407196a28327ad49b51028fd3bde2c595a5858
ThanhHaiKhong <khongthanhhai@gmail.com>
2025-07-31 11:56:04 +0700

    Context Menu's delete section

--- Sources/ContextMenuBuilder/ContextualMenu+Section.swift
+++ Sources/ContextMenuBuilder/ContextualMenu+Section.swift
@@ -179,6 +179,13 @@ extension ContextualMenu.Section {
 	) {
 		Item.action(.remove)
 	}
+	
+	public static let delete = ContextualMenu.Section(
+		id: .delete,
+		options: [.displayInline]
+	) {
+		Item.action(.deleteFromLibrary)
+	}
 }
 
 // MARK: - Section IDs
@@ -189,7 +196,8 @@ extension ContextualMenu.Section.ID {
 	public static let share = ContextualMenu.Section.ID("shareSection")
 	public static let queue = ContextualMenu.Section.ID("queueSection")
 	public static let remove = ContextualMenu.Section.ID("removeSection")
-	public static let favorite = ContextualMenu.Section.ID("removeSection")
+	public static let favorite = ContextualMenu.Section.ID("favoriteSection")
+	public static let delete = ContextualMenu.Section.ID("deleteSection")
 }
 
 // MARK: - Builder
